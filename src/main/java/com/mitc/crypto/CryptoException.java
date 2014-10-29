e4ded5802f82cbb3f5fa006a5667dcb54b528af2
mortias <mortier@telenet.be>
2014-10-29 20:38:45 +0100

    New changelist

---
+++ src/main/java/com/mitc/crypto/CryptoException.java
@@ -0,0 +1,11 @@
+package com.mitc.crypto;
+
+public class CryptoException extends Exception {
+
+    public CryptoException() {
+    }
+
+    public CryptoException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
