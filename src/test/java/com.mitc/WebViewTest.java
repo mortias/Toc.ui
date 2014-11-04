ac3f8b6d9cea8c3040bc30da95f4905b80ebdb89
mortias <mortias>
2014-11-04 10:14:18 +0100

    commit

---
+++ src/test/java/com.mitc/WebViewTest.java
@@ -0,0 +1,40 @@
+package com.mitc;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.net.URISyntaxException;
+
+public class WebViewTest extends Application {
+
+    private Pane root;
+
+    @Override
+    public void start(final Stage stage) throws URISyntaxException {
+
+        final WebView webView = new WebView();
+        final WebEngine webEngine = webView.getEngine();
+
+        webEngine.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0");
+        webEngine.load("http://cf8rtdd1.cc.cec.eu.int:6090/ercbackoffice/index.cfm");
+
+        Scene scene = new Scene(webView);
+
+        stage.setScene(scene);
+        stage.setWidth(1200);
+        stage.setHeight(600);
+        stage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
