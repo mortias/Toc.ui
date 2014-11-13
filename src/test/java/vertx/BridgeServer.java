49bc1d640ff55148d8cde5e3b28e11c993ffc393
mortias <mortias>
2014-11-13 15:43:52 +0100

    commit

--- src/test/java/vertx/BridgeServer.java
+++ src/test/java/vertx/BridgeServer.java
@@ -1,7 +1,9 @@
 package vertx;
 
+import org.vertx.java.core.Handler;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.core.VertxFactory;
+import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.core.json.JsonObject;
@@ -12,19 +14,35 @@ public class BridgeServer {
     public static synchronized void main(String[] args) throws Exception {
 
         Vertx vertx = VertxFactory.newVertx();
-        HttpServer server = vertx.createHttpServer();
 
         JsonArray permitted = new JsonArray();
         permitted.add(new JsonObject()); // Let everything through
 
-        ServerHook hook = new ServerHook();
-        SockJSServer sockJSServer = vertx.createSockJSServer(server);
-        sockJSServer.setHook(hook);
-        sockJSServer.bridge(new JsonObject().putString("prefix", "/eventbus"), permitted, permitted);
+        HttpServer server = vertx.createHttpServer();
+
+        SockJSServer sockServer = vertx.createSockJSServer(server);
+        sockServer.bridge(new JsonObject().putString("prefix", "/eventbus"), permitted, permitted);
         server.listen(9356);
+
+        vertx.eventBus().registerHandler("someaddress", new Handler<Message>() {
+            @Override
+            public void handle(Message event) {
+
+                System.out.println("aa" + event.body().toString());
+
+                JsonArray msg = new JsonArray();
+                msg.add(new JsonObject().putString("height", "550"));
+                msg.add(new JsonObject().putString("width", "560"));
+                msg.add(new JsonObject().putString("text", "zzzz"));
+                msg.add(new JsonObject().putString("skin", "cupertino"));
+
+                vertx.eventBus().publish("someaddress2", msg);
+            }
+        });
 
         // Prevent the JVM from exiting
         System.in.read();
+
 
     }
 }
